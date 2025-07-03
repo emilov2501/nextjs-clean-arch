202df8c23aa047f5802b6b1c4474c182705630f3
Eldiiyar Temir <teldiyar@infocom.kg>
2025-07-03 16:32:02 +0600

    update container

--- src/core/container.ts
+++ src/core/container.ts
@@ -1,26 +1,37 @@
 import { LoginUseCase } from "@/features/auth/usecase/LoginUseCase";
+import { GetUsersUseCase } from "@/features/user/usecase/GetUsersUseCase";
 import { AuthRepository } from "@/repositories/AuthRepository";
 import { AuthRepositoryImpl } from "@/repositories/impl/AuthRepositoryIml";
+import { UserRepositoryImpl } from "@/repositories/impl/UserRepositoryImpl";
+import { UserRepository } from "@/repositories/UserRepository";
 import { AuthService } from "@/services/AuthService";
 import { AuthServiceImpl } from "@/services/impl/AuthServiceImpl";
+import { UserServiceImpl } from "@/services/impl/UserServiceImpl";
+import { UserService } from "@/services/UserService";
 import { http } from "@/shared/api/client";
 
 export class AppContainer {
 	private static instance: AppContainer;
 
 	private authRepository: AuthRepository;
+	private userRepository: UserRepository;
+	private userService: UserService;
 	private authService: AuthService;
 	public loginUseCase: LoginUseCase;
+	public getUsersUseCase: GetUsersUseCase;
 
 	private constructor() {
 		// Repositories
 		this.authRepository = new AuthRepositoryImpl(http);
+		this.userRepository = new UserRepositoryImpl(http);
 
 		// Services
 		this.authService = new AuthServiceImpl(this.authRepository);
+		this.userService = new UserServiceImpl(this.userRepository);
 
 		// Usecases
 		this.loginUseCase = new LoginUseCase(this.authService);
+		this.getUsersUseCase = new GetUsersUseCase(this.userService);
 	}
 
 	public static getInstance(): AppContainer {
